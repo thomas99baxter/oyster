9380858c2738ec38c15c70e604f3ccf385568f70
Troy Snowden <troybradsnowden@gmail.com>
2021-09-27 15:27:51 +0100

    Add top up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,4 +3,8 @@ class Oystercard
   def initialize(balance = 0)
     @balance = balance
   end
+
+  def top_up(amount)
+    @balance += amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,24 @@
 require 'oystercard'
 describe Oystercard do
-  it "should initialize the class" do
-    expect(subject.class).to eq(Oystercard)
+  
+  describe "#initialize" do
+    it "should initialize the class" do
+      expect(subject.class).to eq(Oystercard)
+    end
+
+    it "should initialize the balance to 0 if not passed" do
+      expect(subject.balance).to eq(0)
+    end
+
+    it "should initialize the balance to parameter if passed" do
+      expect(described_class.new(10).balance).to eq(10)
+    end
   end
 
-  it "should initialize the class" do
-    expect(subject.balance).to eq(0)
+  describe "#top_up" do
+    it "should top balance by specified amount" do
+      subject.top_up(10)
+      expect(subject.balance).to eq(10)
+    end
   end
 end

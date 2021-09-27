685b389a0fca58b08da9f15860764c52dcc84491
Tom Baxter <thomas99baxter@gmail.com>
2021-09-27 15:19:35 +0100

    feat(oystercard): add initialise and balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,6 @@
 class Oystercard
-  def initialize
-    
+  attr_reader :balance
+  def initialize(balance = 0)
+    @balance = balance
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,10 @@
 require 'oystercard'
 describe Oystercard do
-  
+  it "should initialize the class" do
+    expect(subject.class).to eq(Oystercard)
+  end
+
+  it "should initialize the class" do
+    expect(subject.balance).to eq(0)
+  end
 end
